1f994f7e897cc33b1dca788c8903a89a7030050c
jdlarssen <t7pvhqdtcf@privaterelay.appleid.com>
2025-12-20 22:12:33 +0100

    H2 + testprofile

--- src/test/java/no/jdl/ukemeny/UkemenyApplicationTests.java
+++ src/test/java/no/jdl/ukemeny/UkemenyApplicationTests.java
@@ -2,6 +2,7 @@ package no.jdl.ukemeny;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 class UkemenyApplicationTests {
@@ -11,3 +12,6 @@ class UkemenyApplicationTests {
 	}
 
 }
+@ActiveProfiles("test")
+@SpringBootTest
+class UkemenyApplicationTests { ... }
